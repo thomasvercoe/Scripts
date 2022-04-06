0f31bf4453aa2eff3679ba64fe8f097286fdf42f
thomasvercoe <thvercoe@bigpond.com>
2022-04-06 17:55:10 +1000

    fixed stuff that wasn't broken

--- auto_wpgtk.sh
+++ auto_wpgtk.sh
@@ -10,29 +10,29 @@ wallpapers () {
 
 
 #get monitor
-monitor=$(xrandr|awk '/\<connected/{print "monitor"$1}')
+monitor="$(xrandr|awk '/\<connected/{print "monitor"$1}')"
 
-var_wallpapers=$(wallpapers)
+var_wallpapers="$(wallpapers)"
 
 
 while true; do
 
     #get current workspace
-    workspace=$(xdotool get_desktop)
+    workspace="$(xdotool get_desktop)"
 
 
-    wallpaper=$(echo "$var_wallpapers" | grep "$monitor" | grep "workspace$workspace"| sed -n -e 's/^.*last-image             //p')
+    wallpaper="$(echo "$var_wallpapers" | grep "$monitor" | grep "workspace$workspace"| sed -n -e 's/^.*last-image             //p')"
 
 
 
-    if [[ $(basename "$wallpaper") != "$(wpg -c)" ]]
+    if [[ "$(basename "$wallpaper")" != "$(wpg -c)" ]]
     then
 
         wpg -a "$wallpaper"
         wpg -s "$wallpaper"
 
         wallpapers
-        var_wallpapers=$(wallpapers)
+        var_wallpapers="$(wallpapers)"
 
     fi
 
